a79321aa1fef3005e679095301367988585683ce
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-11-12 14:18:36 +0100

    fix eslint errors

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -4,10 +4,9 @@ import './style.css';
 function component() {
   const element = document.createElement('div');
 
-  //Lodash, now imported by this script
+  // Lodash, now imported by this script
   element.innerHTML = _.join(['', ''], ' ');
   element.classList.add('hello');
-  
 
   return element;
 }
@@ -24,23 +23,42 @@ const list = document.querySelector('.grocery-list');
 const clearBtn = document.querySelector('.clear-list');
 
 // edit option
-let editElement;
-let editFlag = false;
-let editID = '';
+// let editElement;
+const editFlag = false;
+// const editID = '';
 
-// add event listeners
+// clearItems function
+function clearItem() {}
 
-form.addEventListener('submit', addItem);
-clearBtn.addEventListener('click', clearItem);
-window.addEventListener('DOMContentLoaded', setupItems);
+function deleteItem() {}
+function editItem() {}
+
+// functions
+
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  setTimeout(() => {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
+// set back to default
+function setBacktoDefault() {
+  groceryItem.value = '';
+  // editFlag = false;
+  // editID = '';
+  // submitBtn.textContent = 'Submit';
+}
 
 // functions
 
 function addItem(e) {
   e.preventDefault();
-  const value = groceryItem.value;
+  const { value } = groceryItem;
   const id = new Date().getTime().toString();
-  if (!value == '' && editFlag === false) {
+  if (!value === '' && editFlag === false) {
     const element = document.createElement('article');
     element.classList.add('listed-item');
     const attr = document.createAttribute('data-id');
@@ -79,27 +97,8 @@ function addItem(e) {
   }
 }
 
-// functions
+// add event listeners
 
-function displayAlert(text, action) {
-  alert.textContent = text;
-  alert.classList.add(`alert-${action}`);
-  setTimeout(() => {
-    alert.textContent = '';
-    alert.classList.remove(`alert-${action}`);
-  }, 1000);
-}
-
-// set back to default
-function setBacktoDefault() {
-  groceryItem.value = '';
-  // editFlag = false;
-  // editID = '';
-  // submitBtn.textContent = 'Submit';
-}
-
-// clearItems function
-function clearItem() {}
-
-function deleteItem(e) {}
-function editItem(e) {}
+form.addEventListener('submit', addItem);
+clearBtn.addEventListener('click', clearItem);
+window.addEventListener('DOMContentLoaded', setupItems);
