20b28e756dea1218a006b124aee0c87bcb7b57ca
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-11-12 19:39:17 +0100

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: '/src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    
   },
 
   devServer: {
