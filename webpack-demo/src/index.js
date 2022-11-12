c4ac10ade6c686a0bef57ed50b442f1fb5aec332
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-11-12 13:25:27 +0100

    render list dynamically

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -15,3 +15,90 @@ document.body.appendChild(component());
 
 // my code
 
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const groceryItem = document.querySelector('.grocery-item');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-list');
+
+// edit option
+let editElement;
+let editFlag = false;
+let editID = '';
+
+// add event listeners
+
+form.addEventListener('submit', addItem);
+clearBtn.addEventListener('click', clearItem);
+window.addEventListener('DOMContentLoaded', setupItems);
+
+// functions
+
+function addItem(e) {
+  e.preventDefault();
+  const value = groceryItem.value;
+  const id = new Date().getTime().toString();
+  if (!value == '' && editFlag === false) {
+    const element = document.createElement('article');
+    element.classList.add('listed-item');
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `
+                <p class="selected-item">${value}</p>
+
+            <span class="list-btns">
+              <button class="edit-btn">
+                <i class="fa-solid fa-pen-to-square"></i>
+              </button>
+              <button class="close-btn">
+                <i class="fa-solid fa-trash"></i>
+              </button>
+            </span>
+    `;
+    const deleteBtn = element.querySelector('.close-btn');
+    const editBtn = element.querySelector('.edit-btn');
+
+    deleteBtn.addEventListener('click', deleteItem);
+    editBtn.addEventListener('click', editItem);
+
+    // appendchild
+    list.appendChild(element);
+    displayAlert('item added to the list', 'success');
+    container.classList.add('show-container');
+    // set back to default
+    setBacktoDefault();
+
+    // add toLocalStorage
+    addToLocalStorage(id, value);
+  } else if (value !== '' && editFlag) {
+  } else {
+    displayAlert('please enter value', 'danger');
+  }
+}
+
+// functions
+
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+  setTimeout(() => {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
+}
+
+// set back to default
+function setBacktoDefault() {
+  groceryItem.value = '';
+  // editFlag = false;
+  // editID = '';
+  // submitBtn.textContent = 'Submit';
+}
+
+// clearItems function
+function clearItem() {}
+
+function deleteItem(e) {}
+function editItem(e) {}
