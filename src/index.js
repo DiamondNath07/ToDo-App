193ef7b2a1132975ce088e78c6ae3edb84ae0d03
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-11-14 13:30:54 +0100

    render static data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,38 @@
 import './style.css';
-import TodoList from './modules/todoapp.js';
+// import TodoList from './modules/todoapp.js';
+
+const arrofObj = [
+  {
+    index: 1,
+    description: 'Wash the plate',
+    completed: false,
+  },
+  {
+    index: 2,
+    description: 'I want to finish my task',
+    completed: false,
+  },
+];
+
+arrofObj.forEach(({ index, description, completed }) => {
+  const labelContainer = document.querySelector('.label-container');
+
+  const element = document.createElement('div');
+  const attr = document.createAttribute('class');
+  attr.value = 'label';
+  element.setAttributeNode(attr);
+  element.innerHTML = `
+    <input type="checkbox" value="dishes" />
+            <label placeholder='${completed}'>${(index, description)}</label>
+            <span class="icon-more"
+              ><i class="fa-solid fa-ellipsis-vertical"></i
+            ></span>
+            <span class="icon-trash"><i class="fa-solid fa-trash"></i></span>
+            <br />
+   
+   `;
+  labelContainer.appendChild(element);
+});
 
 const todoApp = new TodoList();
 const form = document.querySelector('.Todo-form');
