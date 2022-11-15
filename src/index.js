6854fb4002e02982d7c813ad4a7945bdfd076649
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-11-15 21:31:01 +0100

    add arrays to the list

--- src/index.js
+++ src/index.js
@@ -1,39 +1,6 @@
-import {showMenu, deleteTask, updateStatus} from './todoapp.js'
-
-const taskInput = document.querySelector('.task-input input'),
-  taskBox = document.querySelector('.task-box');
-
-let todos = JSON.parse(localStorage.getItem('todo-list'));
-
-function showTodo() {
-  let li = '';
-  if (todos) {
-    todos.forEach((todo, id) => {
-      let iscompleted = todo.status == 'completed' ? 'checked' : '';
-
-      li += `
-    <li class="task">
-          <label for="${id}">
-            <input onclick='updateStatus(this)' type="checkbox" id="${id}"  ${iscompleted}/>
-            <p class = '${iscompleted}'>${todo.name}</p>
-          </label>
-          <div class="settings">
-            <i onclick = 'showMenu(this)' class="fa-solid fa-ellipsis-vertical"></i>
-            <ul class="task-menu">
-              <li><i class="fa-solid fa-edit"></i></li>
-              <li><i onclick = 'deleteTask(${id})' class="fa-solid fa-trash"></i></li>
-            </ul>
-          </div>
-        </li>  
-    `;
-    });
-  }
-  taskBox.innerHTML = li;
-}
-
-showTodo();
-
-
+import './style.css';
+import { createTodo, taskInput, todos } from './modules/todoapp';
+console.log(taskInput);
 taskInput.addEventListener('keyup', (e) => {
   let userTask = taskInput.value.trim();
   if (e.key == 'Enter' && userTask) {
@@ -44,9 +11,6 @@ taskInput.addEventListener('keyup', (e) => {
     let taskInfo = { name: userTask, status: 'pending' };
     todos.push(taskInfo);
     localStorage.setItem('todo-list', JSON.stringify(todos));
-    showTodo();
+    createTodo();
   }
 });
-
-import './style.css';
-
--- src/modules/todoapp.js
+++ src/modules/todoapp.js
@@ -1,5 +1,36 @@
+export const taskInput = document.querySelector('.task-input input'),
+  taskBox = document.querySelector('.task-box');
 
-export function showMenu(selectedTask) {
+export let todos = JSON.parse(localStorage.getItem('todo-list'));
+
+export function createTodo() {
+  let li = '';
+  if (todos) {
+    todos.forEach((todo, id) => {
+      let iscompleted = todo.status == 'completed' ? 'checked' : '';
+
+      li += `
+    <li class="task">
+          <label for="${id}">
+            <input onclick='updateStatus(this)' type="checkbox" id="${id}"  ${iscompleted}/>
+            <p class = '${iscompleted}'>${todo.name}</p>
+          </label>
+          <div class="settings">
+            <i onclick = 'showMenu(this)' class="fa-solid fa-ellipsis-vertical option-icon"></i>
+            <ul class="task-menu">
+              <li><i class="fa-solid fa-edit"></i></li>
+              <li><i onclick = 'deleteTask(${id})' class="fa-solid fa-trash"></i></li>
+            </ul>
+          </div>
+        </li>  
+    `;
+    });
+  }
+}
+
+createTodo();
+
+function showMenu(selectedTask) {
   let taskMenu = selectedTask.parentElement.lastElementChild;
   taskMenu.classList.toggle('show');
   document.addEventListener('click', (e) => {
@@ -9,13 +40,13 @@ export function showMenu(selectedTask) {
   });
 }
 
-export function deleteTask(deleteId) {
+function deleteTask(deleteId) {
   todos.splice(deleteId, 1);
   localStorage.setItem('todo-list', JSON.stringify(todos));
-  showTodo();
+  createTodo();
 }
 
-export function updateStatus(selectedTask) {
+function updateStatus(selectedTask) {
   let taskName = selectedTask.parentElement.lastElementChild;
   if (selectedTask.checked) {
     taskName.classList.add('checked');
@@ -26,5 +57,3 @@ export function updateStatus(selectedTask) {
   }
   localStorage.setItem('todo-list', JSON.stringify(todos));
 }
-
-
