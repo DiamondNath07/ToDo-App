87d24fe80cfcd7a2f5d8029fd3ff5847a1794450
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-11-15 10:33:48 +0100

    add delete functionality

--- src/index.js
+++ src/index.js
@@ -1,21 +1,52 @@
-import './style.css';
-import TodoList from './modules/todoapp.js';
+import {showMenu, deleteTask, updateStatus} from './todoapp.js'
 
-const todoApp = new TodoList();
-const form = document.querySelector('.Todo-form');
+const taskInput = document.querySelector('.task-input input'),
+  taskBox = document.querySelector('.task-box');
 
-// window.onload = () => {
-//   todoApp.LoadData();
-//   todoApp.displayActivity();
-// };
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  todoApp.addActivity();
-  todoApp.displayActivity();
-  todoApp.restoreDefault();
-  // todoApp.saveData();
+let todos = JSON.parse(localStorage.getItem('todo-list'));
+
+function showTodo() {
+  let li = '';
+  if (todos) {
+    todos.forEach((todo, id) => {
+      let iscompleted = todo.status == 'completed' ? 'checked' : '';
+
+      li += `
+    <li class="task">
+          <label for="${id}">
+            <input onclick='updateStatus(this)' type="checkbox" id="${id}"  ${iscompleted}/>
+            <p class = '${iscompleted}'>${todo.name}</p>
+          </label>
+          <div class="settings">
+            <i onclick = 'showMenu(this)' class="fa-solid fa-ellipsis-vertical"></i>
+            <ul class="task-menu">
+              <li><i class="fa-solid fa-edit"></i></li>
+              <li><i onclick = 'deleteTask(${id})' class="fa-solid fa-trash"></i></li>
+            </ul>
+          </div>
+        </li>  
+    `;
+    });
+  }
+  taskBox.innerHTML = li;
+}
+
+showTodo();
+
+
+taskInput.addEventListener('keyup', (e) => {
+  let userTask = taskInput.value.trim();
+  if (e.key == 'Enter' && userTask) {
+    if (!todos) {
+      todos = [];
+    }
+    taskInput.value = '';
+    let taskInfo = { name: userTask, status: 'pending' };
+    todos.push(taskInfo);
+    localStorage.setItem('todo-list', JSON.stringify(todos));
+    showTodo();
+  }
 });
 
-let container = document.querySelector('.label-container');
-const val = container.childNodes[1];
-console.log(val);
+import './style.css';
+
--- src/modules/todoapp.js
+++ src/modules/todoapp.js
@@ -1,100 +1,30 @@
-export default class TodoList {
-  constructor() {
-    this.todo = [];
-    this.storagePass = 'todoApp';
-  }
 
-  restoreDefault() {
-    const addtext = document.querySelector('.add-text');
-    addtext.value = '';
-    this.text = '';
-  }
-
-  addActivity() {
-    const addtext = document.querySelector('.add-text');
-    const addList = {
-      id: Date.now(),
-      description: addtext.value,
-      index: this.todo.length + 1,
-      completed: false,
-    };
-
-    this.todo.push(addList);
-  }
-
-  displayActivity() {
-    const labelContainer = document.querySelector('.label-container');
-    const addtext = document.querySelector('.add-text');
-    const { value } = addtext;
-    const element = document.createElement('div');
-    const attr = document.createAttribute('class');
-    attr.value = 'label';
-    element.setAttributeNode(attr);
-    element.innerHTML = `
-    <input type="checkbox" value="dishes" />
-            <label>${value}</label>
-            <span class="icon-more" 
-              ><i class="fa-solid fa-ellipsis-vertical"></i
-            ></span>
-            <span class="icon-trash" ><i class="fa-solid fa-trash"></i></span>
-            <br />
-   `;
-    this.text = '';
-    // console.log(element);
-    labelContainer.appendChild(element);
-
-    // console.log(labelContainer);
-    const target = labelContainer.currentTarget;
-    // console.log(target);
-
-    const icon = (e) => {
-      const value = e.currentTarget;
-      console.log(value);
-    };
-    const opt = document.querySelector('.icon-more');
-    //
-    const trash = document.querySelector('.icon-trash');
-
-    // for (let i = 0; i < opt.length; i++) {
-    //   opt[i].addEventListener('click', () => {
-    //     console.log(opt);
-
-    //     trash.classList.toggle('show');
-    //   });
-    // }
-    opt.addEventListener('click', () => {
-      icon();
-      
-      trash.classList.toggle('show');
-    });
-    if (opt) {
-      opt.map((icon, index) => {
-        console.log(icon);
-      });
+export function showMenu(selectedTask) {
+  let taskMenu = selectedTask.parentElement.lastElementChild;
+  taskMenu.classList.toggle('show');
+  document.addEventListener('click', (e) => {
+    if (e.target.tagName !== 'I' || e.target !== selectedTask) {
+      taskMenu.classList.remove('show');
     }
-    // this.todo.forEach((list) => {
-
-    // });
-
-    // trash.addEventListener('click', () => {
-    //   this.deleteData();
-    // });
-  }
-
-  // deleteData(id) {
-  //   this.todo = this.todo.filter((list) => {
-  //     list.id !== id;
-  //     list = document.getElementById(id);
-  //     console.log(list);
-  //     list.remove(id);
-  //   });
-  // }
-
-  // saveData() {
-  //   localStorage.setItem(this.storagePass, JSON.stringify(this.todo));
-  // }
-
-  // LoadData() {
-  //   this.todo = JSON.parse(localStorage.getItem(this.storagePass));
-  // }
+  });
 }
+
+export function deleteTask(deleteId) {
+  todos.splice(deleteId, 1);
+  localStorage.setItem('todo-list', JSON.stringify(todos));
+  showTodo();
+}
+
+export function updateStatus(selectedTask) {
+  let taskName = selectedTask.parentElement.lastElementChild;
+  if (selectedTask.checked) {
+    taskName.classList.add('checked');
+    todos[selectedTask.id].status = 'completed';
+  } else {
+    taskName.classList.remove('checked');
+    todos[selectedTask.id].status = 'pending';
+  }
+  localStorage.setItem('todo-list', JSON.stringify(todos));
+}
+
+
