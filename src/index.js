61effd9e19703c4b53803dc08c4e096f639af12e
DiamondNath07 <diamondnathaniel2@gmail.com>
2022-11-17 05:21:01 +0100

    add delete function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
-import './style.css';
-import { createTodo, taskInput, todos } from './modules/todoapp';
-console.log(taskInput);
-taskInput.addEventListener('keyup', (e) => {
-  let userTask = taskInput.value.trim();
-  if (e.key == 'Enter' && userTask) {
-    if (!todos) {
-      todos = [];
-    }
-    taskInput.value = '';
-    let taskInfo = { name: userTask, status: 'pending' };
-    todos.push(taskInfo);
-    localStorage.setItem('todo-list', JSON.stringify(todos));
-    createTodo();
+import './styles/styles.css';
+import UiTodo from './modules/uIdisplay.js';
+import MyTodoApp from './modules/myTodoApp.js';
+import Storage from './modules/localStorage.js';
+
+document.addEventListener('DOMContentLoaded', UiTodo.displayToDo);
+document.getElementById('completedBtn').addEventListener('click', () => {
+  Storage.removeCompleted();
+});
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const todoListItem = Storage.getToDo();
+  const toDoInput = document.getElementById('input-text').value;
+  const id = todoListItem.length + 1;
+  const completed = false;
+
+  const todo = new MyTodoApp(toDoInput, id, completed);
+  UiTodo.addToDoList(todo);
+  Storage.addTodo(todo);
+  UiTodo.deleteData();
+});
+document.getElementById('to-do-container').addEventListener('click', (e) => {
+  Storage.editInput(
+    e.target.parentElement.parentElement.children[4],
+    e.target.parentElement,
+    e.target.parentElement.parentElement,
+    e.target.parentElement.parentElement.children[0].children[0],
+  );
+  UiTodo.trashTodo(e.target);
+  if (e.target.classList.contains('check')) {
+    Storage.checkboxCompleted(
+      e.target.parentElement.parentElement.children[4],
+      e.target.checked,
+    );
+    e.target.parentElement.parentElement.children[2].children[0].classList.toggle(
+      'strike-through',
+    );
   }
+  Storage.remove(
+    e.target.parentElement.previousElementSibling.previousElementSibling
+      .textContent,
+  );
 });
